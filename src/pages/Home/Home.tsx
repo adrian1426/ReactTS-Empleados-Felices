2c28a5156a9329a1f6bc0aa7c2fdba4b5fcab271
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-10-20 13:27:13 -0500

    agregando tabla en home - Grid table

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,59 @@
 import React from 'react';
-export interface HomeInterface {}
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+import { People } from '@/data/people';
 
-const Home : React.FC<HomeInterface> = () => {
-	return <div>Home</div>;
+export interface HomeInterface { }
+
+const Home: React.FC<HomeInterface> = () => {
+
+	const pageSize = 5;
+
+	const columns: GridColDef[] = [
+		{
+			field: 'name',
+			headerName: 'nombre',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => (
+				<>
+					{params.value}
+				</>
+			)
+		},
+		{
+			field: 'category',
+			headerName: 'categoria',
+			flex: 1,
+			renderCell: (params: GridRenderCellParams) => (
+				<>
+					{params.value}
+				</>
+			)
+		},
+		{
+			field: 'company',
+			headerName: 'compania',
+			flex: 1,
+			renderCell: (params: GridRenderCellParams) => (
+				<>
+					{params.value}
+				</>
+			)
+		}
+	];
+
+	return (
+		<DataGrid
+			columns={columns}
+			rows={People}
+			disableColumnSelector
+			disableSelectionOnClick
+			autoHeight
+			pageSize={pageSize}
+			rowsPerPageOptions={[pageSize]}
+			getRowId={(row: any) => row.id}
+		/>
+	);
 };
 
 export default Home;
